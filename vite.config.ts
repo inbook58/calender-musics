d63993c399ee275ccc9d2c0fa6e1f05592a59fa3
inbook58 <radiognome2032@gmail.com>
2025-08-10 23:29:48 +0900

    PWA実装

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa' // Import VitePWA
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
@@ -10,6 +11,37 @@ export default defineConfig(({ command }) => {
     plugins: [
       vue(),
       vueDevTools(),
+      VitePWA({ // PWA Configuration
+        registerType: 'autoUpdate',
+        injectRegister: 'autoUpdate',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,json,vue}'],
+        },
+        manifest: {
+          name: 'Calendar Musics',
+          short_name: 'CalMusics',
+          description: 'A calendar of musics',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-icons/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-icons/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-icons/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
     ],
     resolve: {
       alias: {
