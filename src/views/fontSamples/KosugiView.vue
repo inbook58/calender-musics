f358976c74ef288dbff9e03100b583b5fb90932b
inbook58 <radiognome2032@gmail.com>
2025-11-06 14:23:22 +0900

    文章の調整

--- src/views/fontSamples/KosugiView.vue
+++ src/views/fontSamples/KosugiView.vue
@@ -29,20 +29,76 @@ onMounted(() => {
       </section>
     </main>
     <div class="container">
-      <h1 class="heading">破って</h1>
-      <p>あなたが向き合うのは<br>今日だけです<br>
-      前も後もありません</p><br><br>
+      <div class="text-block">
+        <h1 class="heading">
+          <span>破って</span>
+          <span class="en-heading">Tear the pages</span>
+        </h1>
+        <div class="main-text">
+          <p>
+            あなたが向き合うのは<br>
+            今日だけです<br>
+            前も後もありません。
+          </p>
+          <p class="en">
+            What you face is only today<br>
+            —no before, no after.
+          </p>
+        </div>
+      </div>
 
-      <h1 class="heading">見て</h1>
-      <p>よく見てください<br>
-      日付やイラストは<br>毎日少しづつ変わっていきます</p><br><br>
+      <div class="text-block">
+        <h1 class="heading">
+          <span>見て</span>
+          <span class="en-heading">Find the changes</span>
+        </h1>
+        <div class="main-text">
+          <p>
+            よく見てください<br>
+            日付やイラストは<br>
+            毎日少しづつ変わっていきます
+          </p>
+          <p class="en">
+            Pay attention to the page.<br>
+            The dates and illustrations shift,<br>
+            little by little.
+          </p>
+        </div>
+      </div>
 
-      <h1 class="heading">聴いて</h1>
-      <p>一曲づつ並び替えました<br>
-      その曲はその日を<br>象徴するものになるかもしれません</p><br><br>
+      <div class="text-block">
+        <h1 class="heading">
+          <span>聴いて</span>
+          <span class="en-heading">Listen the songs</span>
+        </h1>
+        <div class="main-text">
+          <p>
+            一曲づつ並び替えました<br>
+            その曲はその日を<br>
+            象徴するものになるかもしれません
+          </p>
+          <p class="en">
+            Each song has been carefully selected<br>
+            and reordered.<br>
+            That song might become<br>
+            the one that defines your day.
+          </p>
+        </div>
+      </div>
 
-      <p>いい日とよくない日<br>好きな曲とそうでない曲<br><br>
-      dia diaは<br>あなたの一年を<br>音楽とともに楽しむカレンダー</p><br>
+      <div class="text-block">
+        <div class="main-text">
+          <p>いい日とよくない日<br>好きな曲とそうでない曲</p>
+          <p class="en">Good days and bad days.<br>Songs you love and songs you don’t.</p>
+        </div>
+      </div>
+
+      <div class="text-block">
+        <div class="main-text">
+          <p>dia a dia(一日一日)は<br>あなたの一年を<br>音楽とともに楽しむカレンダー</p>
+          <p class="en">dia dia is a calendar<br>to experience your year,<br>one day at a time,<br>through music.</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -124,9 +180,32 @@ onMounted(() => {
   text-align: center;
 }
 
+.text-block {
+  margin-bottom: 45px;
+}
+
 .heading {
   font-size: 1.8rem;
   margin-bottom: 8px;
   font-weight: bold;
+  line-height: 1.2;
+}
+
+.main-text {
+  margin-top: 16px;
+}
+
+.en {
+  font-size: 0.9rem;
+  /* margin-top: 12px; */
+  opacity: 0.8;
+}
+
+.en-heading {
+  display: block;
+  font-size: 1.1rem;
+  font-weight: normal;
+  opacity: 0.8;
+  /* margin-top: 4px; */
 }
 </style>
