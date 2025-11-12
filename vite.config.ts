8aeac35c86e5fb0a2052c008b1efbbdb73a820b4
inbook58 <radiognome2032@gmail.com>
2025-11-13 03:44:31 +0900

    pwaアイコン修正

--- vite.config.ts
+++ vite.config.ts
@@ -29,17 +29,17 @@ export default defineConfig(({ command }) => {
           scope: base,
           icons: [
             {
-              src: 'public/images/icon_sample.png',
+              src: 'images/icon_sample.png',
               sizes: '192x192',
               type: 'image/png',
             },
             {
-              src: 'public/images/icon_sample.png',
+              src: 'images/icon_sample.png',
               sizes: '512x512',
               type: 'image/png',
             },
             {
-              src: 'public/images/icon_sample.png',
+              src: 'images/icon_sample.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable',
