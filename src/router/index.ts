9c79bf871566ae5b8e89c2ccad60c36dacac4a3e
inbook58 <radiognome2032@gmail.com>
2025-11-15 02:19:28 +0900

    本番エラー修正

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import SongPage from '@/views/SongView.vue'
 import Today from '@/views/SongListView.vue'
@@ -7,7 +7,7 @@ import HowToUseView from '@/views/HowToUseView.vue'
 import ShopView from '@/views/ShopView.vue'
 
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
